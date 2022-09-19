c2de511c1072e2b65c5e067d5e4b876df1edc853
Yuji Teshima <yuji.teshima@gmail.com>
2022-09-19 18:12:11 +0900

    add maps delete

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -41,6 +41,30 @@ func TestAdd(t *testing.T) {
 	})
 }
 
+func TestUpdata(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
@@ -65,5 +89,17 @@ func assertDefinition(t testing.TB, dictionary Dictionary, word, definition stri
 	}
 	if definition != got {
 		t.Errorf("got %q want %q", got, definition)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
 	}
 }
